d604a017b923b72038ff8ba6a8a3ae2e5bb44f0e
sen wang <roxwang@foxmail.com>
2018-09-08 10:20:16 +0800

    First day commit!

--- Tencent/src/com/tencent/chapter1/Test1.java
+++ Tencent/src/com/tencent/chapter1/Test1.java
@@ -3,11 +3,14 @@ package com.tencent.chapter1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * @author wangsen
  * @create 2018-09-08 9:02
+ *
+ * title : 两数之和
+ *      给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ *      你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
  */
 public class Test1 {
 
